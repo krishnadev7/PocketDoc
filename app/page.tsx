dabc502c0e17665d73d5369eac24e50903261e99
krishnadev7 <krishnadevv12@gmail.com>
2024-07-07 22:40:06 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-xl text-white">Hello</h1>
+      <Button className="text-black bg-white">Click me</Button>
+    </div>
+  )
+    
+}
