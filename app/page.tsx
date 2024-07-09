5f6bb2ed4c98c7406c837b042e72b08208a66aa5
krishnadev7 <krishnadevv12@gmail.com>
2024-07-09 16:19:58 +0530

    added onboarding and shadcn forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
-import { Button } from "@/components/ui/button";
+import PatientForms from "@/components/forms/PatientForms";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-xl text-white">Hello</h1>
-      <Button className="text-black bg-white">Click me</Button>
+    <div className="flex h-screen max-h-screen">
+        <section className="remove-scrollbar container my-auto">
+            <div className="sub-container max-w-[496px]">
+
+              <div className="flex items-center gap-2">
+                <Image
+                  src={"/assets/images/logo1.jpeg"}
+                  height={1000}
+                  width={1000}
+                  alt="logo"
+                  className="mb-12 h-16 w-fit"
+                />
+                <p>PocketDoc</p>
+              </div>
+
+              <PatientForms/>
+
+
+              <div className="mt-20 text-14-regular flex justify-between">
+                <p className="justify-items-end text-dark-600 xl:text-left">© {(new Date()).getFullYear()} PocketDoc</p>
+                <Link href={"/?admin=true"} className="text-yellow-500">Admin</Link>
+              </div>
+            </div>
+        </section>
+        <Image
+          src={'/assets/images/onboarding-img.png'}
+          height={10000}
+          width={10000}
+          alt="onboarding image"
+          className="max-w-[50%] side-img"
+        >
+        </Image>
     </div>
   )
     
