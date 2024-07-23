6db17e057993140055492c954f5e5ad8ff1621cd
krishnadev7 <krishnadevv12@gmail.com>
2024-07-23 21:33:51 +0530

    selecting physician and Select type is added

--- components/customFormField.tsx
+++ components/customFormField.tsx
@@ -20,6 +20,7 @@ import PhoneInput from "react-phone-number-input";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
+import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
 
 interface CustomProps {
   control: Control<any>,
@@ -87,6 +88,21 @@ const RenderField = ({ field, props }: { field: any, props: CustomProps }) => {
           </FormControl>
         </div>
       )
+      case FormFieldTypes.SELECT:
+      return (
+        <FormControl>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger className="shad-select-trigger">
+                <SelectValue placeholder={props.placeholder} />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent className="shad-select-content">
+              {props.children}
+            </SelectContent>
+          </Select>
+        </FormControl>
+      );
     case FormFieldTypes.SKELETON:
       return (
         renderSkeleton ? renderSkeleton(field) : null
--- components/forms/RegisterForms.tsx
+++ components/forms/RegisterForms.tsx
@@ -12,8 +12,10 @@ import { useRouter } from "next/navigation"
 import { z } from "zod"
 import { FormFieldTypes } from "./PatientForms"
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
-import { GenderOptions } from "@/constants"
+import { Doctors, GenderOptions } from "@/constants"
 import { Label } from "../ui/label"
+import { SelectItem } from "../ui/select"
+import Image from "next/image"
 
 const RegisterForm = ({ user }: { user: User }) => {
     const router = useRouter();
@@ -167,6 +169,35 @@ const RegisterForm = ({ user }: { user: User }) => {
                         placeholder="+91 9325232542"
                     />
                 </div>
+
+                <section className="space-y-6">
+                    <div className="mb-9 space-y-1">
+                        <h2 className="sub-header">Medical Information</h2>
+                    </div>
+                </section>
+
+                <CustomFormField
+                        control={form.control}
+                        fieldType={FormFieldTypes.SELECT}
+                        name="primaryPhysician"
+                        label="Primary Physician"
+                        placeholder="Select a Physician"
+                >
+                    {Doctors.map((doctor) => (
+                        <SelectItem key={doctor.name} value={doctor.name}>
+                                <div className="flex cursor-pointer items-center gap-2">
+                                    <Image
+                                        src={doctor.image}
+                                        alt="doctor image"
+                                        width={32}
+                                        height={32}
+                                        className="rounded-full border border-dark-500"
+                                    />
+                                    <p>{doctor.name}</p>
+                                </div>
+                        </SelectItem>
+                    ))}
+                </CustomFormField>
 
                 <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
 
