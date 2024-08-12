2cdb68835bdf875b5c7976b27259a2802d0083d9
krishnadev7 <krishnadevv12@gmail.com>
2024-08-12 12:31:04 +0530

    added appointmentModal in table

---
+++ components/AppointmentModal.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { useState } from "react"
+import { Button } from "./ui/button"
+  
+const AppointmentModal = ({type}:{type: 'schedule' | 'cancel'}) => {
+    const [open,setOpen] = useState(false)
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+    <DialogTrigger asChild>
+        <Button variant="ghost" className={`capitalize ${type === 'schedule' && 'text-green-500'}`}>{type}</Button>
+    </DialogTrigger>
+    <DialogContent className="shad-dialog sm:max-w-md">
+      <DialogHeader className="mb-4 space-y-3">
+        <DialogTitle className="capitalize">{type} Appointment</DialogTitle>
+        <DialogDescription>
+            Please fill in the following details to {type} an appointment
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
+  
+  )
+}
+
+export default AppointmentModal
